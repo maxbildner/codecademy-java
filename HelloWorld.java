56ce350c6ee647dcf5471ba38ec0222b67f8097d
mbildner <mbildner@icloud.com>
2020-08-12 22:06:44 -0400

    upload L1 notes hello world

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+// NOTES
+// - class name must match file name "HelloWorld.java" (case sensitive)
+// - Java is a COMPILED language = code is transformed into byte code by a
+//   compiler before it is executed. 
+// - JavaScript is a INTERPRETED language = no compiler. but code is read
+//   and executed by some other program
+// - can compile java in mac terminal with  "javac [filename].java"
+//    - this creates a .class file in your current directory, which you can 
+//      then run with the below command:
+// - can run java in mac terminal with      "java [filename]"
+// - Java files must have at least one class AND one main() method
+// - main() 
+//    - runs the main task of the program
+
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
